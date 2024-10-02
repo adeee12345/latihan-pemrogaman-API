b04f7522b1530ea5183892aa5544338d68c88fad
adeee12345 <yeftairmanto.com>
2024-10-02 13:29:58 +0700

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
-import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-   <h1>TESTING</h1>
+    <>
+      <h1>TESTING</h1><br />
+      <Link href="/posts">POSTING PAGE</Link><br />
+      <Link href="/albums">ALBUM PAGE</Link>
+    </>
   );
 }
